ca657ff8b8c035512d26a156dd3ac1d68e724f6b
qiyuliu <qiyuliu@kedacom.com>
2022-07-19 14:31:13 +0800

    commit

---
+++ demo/src/com/github/qyl/StringTest.java
@@ -0,0 +1,16 @@
+package com.github.qyl;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String s = new String("1");
+        s.intern();
+        String s2 = "1";
+        System.out.println(s == s2); //false
+
+        String s3 = new String("1") + new String("2");
+        s3.intern();
+        String s4 = "12";
+        System.out.println(s3 == s4); //true
+    }
+}
