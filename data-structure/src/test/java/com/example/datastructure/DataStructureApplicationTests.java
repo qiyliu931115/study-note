051651d2a74f2910df6949a8540ca55cfad9303b
镠镠 <15705168506@163.com>
2022-08-05 00:45:04 +0800

    commit

---
+++ data-structure/src/test/java/com/example/datastructure/DataStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
