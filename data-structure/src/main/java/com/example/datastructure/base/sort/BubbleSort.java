1b596617013369a689fb00c8ab179227ba8547bb
镠镠 <15705168506@163.com>
2022-08-13 22:12:25 +0800

    commit

--- data-structure/src/main/java/com/example/datastructure/base/sort/BubbleSort.java
+++ data-structure/src/main/java/com/example/datastructure/base/sort/BubbleSort.java
@@ -7,9 +7,11 @@ import java.util.Date;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        //int[] arr = new int[]{3,9,-1,10,20};
+        int[] arr = new int[]{3,9,-1,10,20};
 //        int[] arr = new int[]{1,2,3,4,5,6};
 //        bubbleSort(arr);
+
+        bubbleSort2(arr);
 
         int[] arr2 = new int[80000];
         for (int i = 0; i < 80000 ;i++) {
@@ -100,4 +102,23 @@ public class BubbleSort {
          }
          //System.out.println("排序结果" + Arrays.toString(arr));
      }
+
+
+    public static void bubbleSort2(int[] arr) {
+        for (int i =0; i < arr.length - 1; i++) {
+
+            for (int j =0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
+            }
+            System.out.println("第" + (i+1) + "排序结果" + Arrays.toString(arr));
+        }
+        System.out.println("排序结果" + Arrays.toString(arr));
+
+    }
 }
