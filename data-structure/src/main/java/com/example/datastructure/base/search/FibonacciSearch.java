ba9e788ad2fa4ed3489998c224580caf56f6824b
qiyuliu <qiyuliu@kedacom.com>
2022-08-19 15:54:24 +0800

    commit

--- data-structure/src/main/java/com/example/datastructure/base/search/FibonacciSearch.java
+++ data-structure/src/main/java/com/example/datastructure/base/search/FibonacciSearch.java
@@ -8,6 +8,10 @@ public class FibonacciSearch {
         int[] arr = {1,8,10,89,1000,1234};
     }
 
+    /**
+     * 得到斐波那契数列  mid = low + F(k-1)-1   F就是fib()方法返回值
+     * @return
+     */
     public static int[] fib () {
 
         int [] f = new int[maxSize];
@@ -18,5 +22,12 @@ public class FibonacciSearch {
             f[i] = f[i -1] + f[i -2];
         }
         return f;
+    }
+
+
+
+    public static int fibonacciSearch(int [] a, int key) {
+        int low =0, high = a.length-1;
+        int k = 0;//表示斐波那契数列分割数值下标
     }
 }
