a4929c5375c84e90c7c2fe407a27ec71e4e9a9bf
qiyuliu <qiyuliu@kedacom.com>
2022-09-05 17:04:36 +0800

    commit

--- demo/src/com/github/qyl/BlockingQueueDemo.java
+++ demo/src/com/github/qyl/BlockingQueueDemo.java
@@ -2,20 +2,18 @@ package com.github.qyl;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class BlockingQueueDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
 
-        System.out.println(blockingQueue.offer("a"));
-        System.out.println(blockingQueue.offer("b"));
-        System.out.println(blockingQueue.offer("c"));
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
 
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());
-        System.out.println(blockingQueue.poll());
 
     }
 
