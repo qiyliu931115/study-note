56fd83e59a23ade8eeba0fbc78e682ca4e800375
qiyuliu <qiyuliu@kedacom.com>
2022-09-07 14:52:20 +0800

    commit

---
+++ demo/src/com/github/qyl/CustomThreadPoolDemo.java
@@ -0,0 +1,27 @@
+package com.github.qyl;
+
+import java.util.concurrent.*;
+
+public class CustomThreadPoolDemo {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = new ThreadPoolExecutor(
+                2, 5,1,TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(3),Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardOldestPolicy());
+
+        //模拟十个用户办理业务
+        try{
+            for (int i = 1;i <=9;i++) {
+                int finalI = i;
+                executorService.execute(()->{
+                    System.out.println(Thread.currentThread().getName() + "办理业务:" + finalI);
+                });
+                //TimeUnit.MILLISECONDS.sleep(200);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            executorService.shutdown();
+        }
+    }
+}
