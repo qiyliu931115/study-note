6d59a00e6de149423cd5ae2217ca7d5e6d857590
qiyuliu <qiyuliu@kedacom.com>
2022-09-16 14:54:13 +0800

    commit

---
+++ demo/src/com/github/qyl/demo.java
@@ -0,0 +1,11 @@
+package com.github.qyl;
+
+public class demo {
+
+    public static void main(String[] args) {
+        int i = 1;
+        i = i++;
+        int j = i ++;
+        System.out.println(i + "," + j);
+    }
+}
