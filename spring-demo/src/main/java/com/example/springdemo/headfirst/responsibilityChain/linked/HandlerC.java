39be11c593121154da6a61a87527e7ba8bbfb947
yuliu.qi <yuliu.qi@tineco.com>
2026-09-22 14:57:06 +0800

    commit

--- spring-demo/src/main/java/com/example/springdemo/headfirst/responsibilityChain/linked/HandlerC.java
+++
@@ -1,8 +0,0 @@
-package com.example.springdemo.headfirst.responsibilityChain.linked;
-
-public class HandlerC extends  Handler{
-    @Override
-    void doHandler(String args) {
-        System.out.println("HandlerC " + args);
-    }
-}
--- spring-demo/src/main/java/com/example/springdemo/headfirst/responsibilityChain/list/IHandler.java
+++
@@ -1,6 +0,0 @@
-package com.example.springdemo.headfirst.responsibilityChain.list;
-
-public interface IHandler {
-
-    boolean handler(String args);
-}
